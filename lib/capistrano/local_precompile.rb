4a4181fca111e1c0b3350898fbd2e18157bd24e5
Joel M.M <theoneomega@gmail.com>
2018-09-17 10:55:31 -0600

    fix on rsync user, now it use the deploy user and his ssh key to sync files

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -38,8 +38,8 @@ namespace :deploy do
     task :rsync do
       on roles(fetch(:assets_role)) do |server|
         run_locally do
-          execute "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:assets_dir)}/" if Dir.exists?(fetch(:assets_dir))
-          execute "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:packs_dir)}/" if Dir.exists?(fetch(:packs_dir))
+          execute "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{fetch(:user)}@#{server.hostname}:#{release_path}/#{fetch(:assets_dir)}/" if Dir.exists?(fetch(:assets_dir))
+          execute "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{fetch(:user)}@#{server.hostname}:#{release_path}/#{fetch(:packs_dir)}/" if Dir.exists?(fetch(:packs_dir))
         end
       end
     end
